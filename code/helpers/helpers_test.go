8a28162cb15d7d4dc80a98d17eb476630652c9ab
Leo <you@example.com>
2021-01-21 22:40:02 +0000

    helpers: added test for ReadFileLineByLine

--- code/helpers/helpers_test.go
+++ code/helpers/helpers_test.go
@@ -11,6 +11,20 @@ func TestReadFile(t *testing.T) {
 	}
 	if expectedOutput != calculatedOutput {
 		t.Errorf("Output mismatch. Expected %s. Got %s", expectedOutput, calculatedOutput)
-		// t.Errorf(calculatedOutput)
 	}
+}
+
+func TestReadFileLineByLine(t *testing.T) {
+	input := "helpers.txt"
+	expectedOutput := []string{"this", "is", "a", "test"}
+	calculatedOutput, err := ReadFileLineByLine(input)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	for index := range expectedOutput {
+		if expectedOutput[index] != calculatedOutput[index] {
+			t.Errorf("Output mismatch. Expected %s. Got %s", expectedOutput[index], calculatedOutput[index])
+		}
+	}
+
 }
