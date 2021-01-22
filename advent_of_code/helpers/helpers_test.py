8ad19a3cddd46153fdfebba799b2bf03505fc4eb
Leo <you@example.com>
2021-01-22 00:05:15 +0000

    day 1 wip

--- advent_of_code/helpers/helpers_test.py
+++ advent_of_code/helpers/helpers_test.py
@@ -5,11 +5,11 @@ from .helpers import read_file, read_file_line_by_line
 class TestHelpers(unittest.TestCase):
     def test_read_file(self) -> None:
         self.assertEqual(
-            read_file(filepath="code/helpers/helpers.txt"),
+            read_file(filepath="advent_of_code/helpers/helpers.txt"),
             "this\nis\na\ntest\n"
         )
     def test_read_file_line_by_line(self) -> None:
         self.assertEqual(
-            read_file_line_by_line(filepath="code/helpers/helpers.txt"),
+            read_file_line_by_line(filepath="advent_of_code/helpers/helpers.txt"),
             ["this", "is", "a", "test"]
         )
