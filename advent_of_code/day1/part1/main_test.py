0d8a1d4c2d56eeef121fba5b1b8e895cfabf1283
Leo <you@example.com>
2021-02-01 19:10:58 +0000

    fixed tests

--- advent_of_code/day1/part1/main_test.py
+++ advent_of_code/day1/part1/main_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from typing import List, Tuple
 
-from advent_of_code.day1.part1 import check_sum, make_pairs, main
+from advent_of_code.day1.part1.main import check_sum, make_pairs, main
 
 
 class TestDay1Part1(unittest.TestCase):
@@ -13,10 +13,10 @@ class TestDay1Part1(unittest.TestCase):
 
     def test_make_pairs(self) -> None:
         input_data: List[int] = [1, 2, 3]
-        expected_output: List[List[int]] = [[1, 2], [1, 3], [2, 3]]
+        expected_output: List[List[int]] = [[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]]
         self.assertEqual(make_pairs(data=input_data), expected_output)
 
     def test_main(self) -> None:
-        expected_output: Optional[Tuple[int, int, int]] = 299, 1721, 514579
+        expected_output: Optional[Tuple[int, int, int]] = 1721, 299, 514579
         actual_input: Optional[Tuple[int, int, int]] = main(file="advent_of_code/day1/input_test.txt")
         self.assertEqual(actual_input, expected_output)
