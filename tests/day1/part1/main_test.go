9469d9efe741995e179c353356e7550eb46ae134
Leo <you@example.com>
2021-02-07 22:16:24 +0000

    refactored tests into tests folder

--- tests/day1/part1/main_test.go
+++ tests/day1/part1/main_test.go
@@ -1,13 +1,14 @@
 package part1
 
 import (
+	"advent-of-code/advent_of_code/day1/part1"
 	"testing"
 )
 
 func TestCheckSum(t *testing.T) {
 	input1, input2 := 2019, 1
 	expectedOutput := []interface{}{2019, 1, 2019, nil}
-	calculatedOutput1, calculatedOutput2, calculatedOutput3, calculatedOutput4 := CheckSum(input1, input2)
+	calculatedOutput1, calculatedOutput2, calculatedOutput3, calculatedOutput4 := part1.CheckSum(input1, input2)
 	calculatedOutput := []interface{}{calculatedOutput1, calculatedOutput2, calculatedOutput3, calculatedOutput4}
 
 	for i := range calculatedOutput {
@@ -22,7 +23,7 @@ func TestMakePairs(t *testing.T) {
 	// function doesn't currently remove duplicates
 	input := []int{1, 2, 3}
 	expectedOutput := [][]int{{1, 2}, {1, 3}, {2, 1}, {2, 3}, {3, 1}, {3, 2}}
-	calculatedOutput := MakePairs(input)
+	calculatedOutput := part1.MakePairs(input)
 
 	for i, val := range calculatedOutput {
 		for j := range val {
@@ -36,7 +37,7 @@ func TestMakePairs(t *testing.T) {
 
 func TestMain(t *testing.T) {
 	expectedOutput := []int{1721, 299, 514579}
-	co1, co2, co3, calculatedErr := Main("../input_test.txt")
+	co1, co2, co3, calculatedErr := part1.Main("../../../advent_of_code/day1/input_test.txt")
 	calculatedOutput := []int{co1, co2, co3}
 
 	if calculatedErr != nil {
